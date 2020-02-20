7ba9d8821e302a2bde7321fbf8d4840d15a3c8c5
sevennight <huc_sevennight@163.com>
2020-02-20 11:11:46 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://xueqiu.com/performance
+// @grant        none
+// @updateURL    http://tool.mamashuojiusuannizhucedeyumingzaichangyehaishihuiyourenfangwen.com/test.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    
+    // Your code here...
+    // this is update 1.1
+})();
